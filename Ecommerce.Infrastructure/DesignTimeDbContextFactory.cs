d1cc2cce63a71521f45db01de333a3fbf5974922
MarteDevs <i202331381@cibertec.edu.pe>
2025-04-23 22:28:32 -0500

    agregamos la funcion de payment

--- Ecommerce.Infrastructure/DesignTimeDbContextFactory.cs
+++ Ecommerce.Infrastructure/DesignTimeDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace Ecommerce.Infrastructure
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server=localhost;Database=EcommerceDB;User Id=sa;Password=sql;TrustServerCertificate=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EcommerceDB;User Id=dev_mps;Password=polo1266;TrustServerCertificate=True;MultipleActiveResultSets=true");
 
             return new AppDbContext(optionsBuilder.Options);
         }
